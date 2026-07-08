ebda413be35bad0549ecf70559acebbef5b29172
Amey Gupta <ameygupta007@yahoo.com>
2026-07-08 14:12:24 +0100

    added more operations

--- minigrad.py
+++ minigrad.py
@@ -1,3 +1,5 @@
+import math
+
 class Value:
 
     def __init__(self, data, children=(), op='', label=''):
@@ -45,8 +47,41 @@ class Value:
     def __rmul__(self, other):
         return self * other
     
-    def backward(self):
+    def __neg__(self):
+        return self * -1.0
+    
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __truediv__(self, other):
+        return self * other**-1
+
+    def __pow__(self, other): # other should be a constant, not Value
+        assert isinstance(other, (int, float)), "only supporting int/float powers"
+        out = Value(self.data**other, (self,), f'**{other}')
+        def _backward():
+            self.grad += other * self.data ** (other -1) * out.grad
+        out._backward = _backward
+
+        return out
+    
+    def exp(self):
+        out = Value(math.exp(self.data), (self, ), 'exp')
+        def _backward():
+            self.grad += out.data * out.grad
         
+        out._backward = _backward
+        return out
+
+    def tanh(self):
+        x = self.data
+        out = Value((math.exp(2*x) - 1) / (math.exp(2*x) + 1), (self,), 'tanh')
+        def _backward():
+            self.grad += (1 - out.data**2) * out.grad
+        out._backward = _backward
+        return out
+
+    def backward(self):
         self.grad = 1
 
         topo = []
